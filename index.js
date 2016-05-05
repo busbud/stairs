0916b2ee04dd7b86b9d20203c3defc4328bb7a5c
Valérian Galliat <val@codejam.info>
2016-05-05 18:05:50 -0400

    Implement persist logic

--- index.js
+++ index.js
@@ -1,32 +1,65 @@
+const uuid = require('node-uuid')
+const Sequelize = require('sequelize')
+const models = require('./src/models')
+
 function Stairs (config) {
   const stairs = {}
   const sessions = {}
+  const db = new Sequelize(config.db)
+  const { User, Session, Run } = models(db)
+
+  let currentThread
+
+  function onError (err) {
+    console.error(`${new Date()} ${err.stack || err}`)
+  }
 
   function onStairs (message) {
-    sessions[message.thread] = {}
+    currentThread = message.thread || uuid.v4()
+
+    return Session.create()
+      .then(session => {
+        sessions[currentThread] = session
+      })
   }
 
   function onDone (message) {
-    if (!sessions[message.thread]) {
+    const session = sessions[message.thread || currentThread]
+
+    if (!session) {
       return message.reply('There was no stairs session here!')
     }
 
     const number = message.words[message.words.indexOf('#done') + 1]
     const floors = number && number.match(/^\d+/) ? Number(number) : config.floors
 
-    message.reply(`You did ${floors} floors!`)
+    return User.upsert({
+      id: message.author.id,
+      name: message.author.name
+    })
+      .then(() => {
+        return Run.create({
+          floors,
+          userId: message.author.id,
+          sessionId: session.id
+        })
+      })
   }
 
   function onMessage (message) {
-    message = Object.assign({}, message, { words: message.text.split(/ +/) })
+    message = Object.assign({}, message, { words: message.text.split(/(?::|,)? +/) })
 
     if (message.words.includes('@team') && message.words.includes('#stairs')) {
-      onStairs(message)
+      return onStairs(message)
+        .catch(onError)
     }
 
     if (message.words.includes('#done')) {
-      onDone(message)
+      return onDone(message)
+        .catch(onError)
     }
+
+    return Promise.resolve()
   }
 
   if (!config.adapter) {
@@ -34,6 +67,17 @@ function Stairs (config) {
     stairs.onMessage = onMessage
   } else {
     config.adapter.on('message', onMessage)
+  }
+
+  stairs.create = () =>
+    db.sync({ force: true })
+
+  stairs.end = () => {
+    db.close()
+
+    if (config.adapter) {
+      config.adapter.end()
+    }
   }
 
   return stairs
---
+++ test/index.js
@@ -0,0 +1,31 @@
+const Stairs = require('../')
+
+const stairs = Stairs({
+  floors: 9,
+  db: 'postgres://localhost/stairs'
+})
+
+const foo = { id: '1', name: 'foo' }
+const bar = { id: '2', name: 'bar' }
+
+function send (message) {
+  return stairs.onMessage(Object.assign({
+    send: message => Promise.resolve(message),
+    reply: message => Promise.resolve(message)
+  }, message))
+}
+
+stairs.create()
+  .then(() => send({
+    author: foo,
+    text: '@team, #stairs'
+  }))
+  .then(() => send({
+    author: foo,
+    text: '#done 4'
+  }))
+  .then(() => send({
+    author: bar,
+    text: '#done'
+  }))
+  .then(() => stairs.end())
